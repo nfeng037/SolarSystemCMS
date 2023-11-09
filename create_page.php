41cdd9224804c1c13172b440bff0fb451539c1fb
Na Feng <nfeng@academic.rrc.ca>
2023-11-08 20:33:46 -0600

    database and image
    The database table has been modified and the images table has been added. Implemented adding images to the page via form upload.

--- create_page.php
+++ create_page.php
@@ -3,75 +3,113 @@
 
 session_start();
 
-require 'db_connect.php'; // 数据库连接
-require 'check_access.php'; // 用户角色检查
+require 'db_connect.php'; 
+require 'check_access.php';
 
-// 如果用户未登录或不是管理员，则重定向到登录页面
+// Redirect to the login page if the user is not logged in or not an admin
 if (!isset($_SESSION['user_id']) || !checkUserRole('admin')) {
     header("Location: login.php");
     exit;
 }
 
-$error = ''; // 初始化错误消息变量
-$success = ''; // 初始化成功消息变量
+$error = ''; // Initialize error message variable
+$success = ''; // Initialize success message variable
 
-// 获取所有分类
+// Get all categories for celestial bodies
 $stmt = $pdo->query("SELECT category_id, category_name FROM categories");
 $categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-// 处理表单提交
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $title = trim($_POST['title']);
-    $content = trim($_POST['content']);
-    $category_id = $_POST['category'];
+// Function to check if the file is an image
+function file_is_an_image($temporary_path, $new_path) {
+    $allowed_mime_types      = ['image/gif', 'image/jpeg', 'image/png'];
+    $allowed_file_extensions = ['gif', 'jpg', 'jpeg', 'png'];
 
-    // 处理图片上传
-    $imageName = '';
-    if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
-        $image = $_FILES['image'];
-        $imagePath = 'uploads/' . basename($image['name']);
-        $imageExtension = pathinfo($imagePath, PATHINFO_EXTENSION);
-        $allowedTypes = ['jpg', 'jpeg', 'png', 'gif'];
-        
-        // 检查文件类型是否为图片
-        if (in_array(strtolower($imageExtension), $allowedTypes) && getimagesize($image['tmp_name'])) {
-            if (move_uploaded_file($image['tmp_name'], $imagePath)) {
-                // 图片上传成功，设置图片名称以便保存到数据库
-                $imageName = $image['name'];
-            } else {
-                $error = 'There was an error uploading the file.';
-            }
-        } else {
-            $error = 'Please upload a valid image file.';
-        }
-    }
+    $actual_file_extension   = strtolower(pathinfo($new_path, PATHINFO_EXTENSION));
+    $actual_mime_type        = getimagesize($temporary_path)['mime'];
 
-    // 如果没有错误，则插入新页面到数据库
-    if (!$error) {
-        try {
-            $pdo->beginTransaction();
-            
-            // 插入新页面
-            $stmt = $pdo->prepare("INSERT INTO pages (title, content, category_id) VALUES (:title, :content, :category_id )");
-            $stmt->execute([':title' => $title, ':content' => $content, ':category_id' => $category_id, ]);
-            $pageId = $pdo->lastInsertId();
+    $file_extension_is_valid = in_array($actual_file_extension, $allowed_file_extensions);
+    $mime_type_is_valid      = in_array($actual_mime_type, $allowed_mime_types);
 
-            // 如果上传了图片，将图片信息插入到 images 表
-            if ($imageName) {
-                $stmt = $pdo->prepare("INSERT INTO images (page_id, file_name) VALUES (:page_id, :file_name)");
-                $stmt->execute([':page_id' => $pageId, ':file_name' => $imageName]);
-            }
-
-            $pdo->commit();
-            $success = 'Page created successfully!';
-        } catch (PDOException $e) {
-            $pdo->rollBack();
-            $error = 'Database error: ' . $e->getMessage();
-        }
-    }
+    return $file_extension_is_valid && $mime_type_is_valid;
 }
 
+// Handle the celestial body creation form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = trim($_POST['name']);
+    $description = trim($_POST['description']);
+    $category_id = $_POST['category'];
+    $imageFilename = ''; // Initialize image filename variable
+
+    // Begin database transaction
+    $pdo->beginTransaction();
+
+    try {
+        // Insert new celestial body into the database
+        $stmt = $pdo->prepare("INSERT INTO celestial_bodies (name, description, category_id) VALUES (:name, :description, :category_id)");
+        $stmt->execute([
+            ':name' => $name,
+            ':description' => $description,
+            ':category_id' => $category_id
+        ]);
+        $celestial_body_id = $pdo->lastInsertId();
+
+        // Insert a corresponding page into the pages table
+        $stmt = $pdo->prepare("INSERT INTO pages (title, content, category_id, creator_id) VALUES (:title, :content, :category_id, :creator_id)");
+        $stmt->execute([
+            ':title' => $name,
+            ':content' => $description,
+            ':category_id' => $category_id,
+            ':creator_id' => $_SESSION['user_id']
+        ]);
+        $page_id = $pdo->lastInsertId();
+
+        // Handle the image upload if a file was provided
+        if (isset($_FILES['image']) && $_FILES['image']['error'] == 0) {
+            $image = $_FILES['image'];
+            $temporaryPath = $image['tmp_name'];
+            $imageFilename = basename($image['name']);
+            $newPath = 'uploads/' . $imageFilename;
+        
+            if (file_is_an_image($temporaryPath, $newPath)) {
+                if (move_uploaded_file($temporaryPath, $newPath)) {
+                    // Image uploaded successfully
+                    // Insert image information into the images table with reference to the page
+                    $stmt = $pdo->prepare("INSERT INTO images (page_id, file_name) VALUES (:page_id, :file_name)");
+                    $stmt->execute([
+                        ':page_id' => $page_id, // Use the page_id from the pages table
+                        ':file_name' => $imageFilename
+                    ]);
+                    
+                    // Update the celestial_bodies table with the image URL
+                    $stmt = $pdo->prepare("UPDATE celestial_bodies SET image_url = :image_url WHERE celestial_body_id = :celestial_body_id");
+                    $stmt->execute([
+                        ':image_url' => $newPath, // The path to the uploaded image
+                        ':celestial_body_id' => $celestial_body_id
+                    ]);
+                } else {
+                    throw new Exception('The file could not be uploaded.');
+                }
+            } else {
+                throw new Exception('The file is not a valid image.');
+            }
+        }
+
+        // Commit the transaction
+        $pdo->commit();
+        $success = 'Celestial body and page created successfully.';
+
+        // Redirect to the view page
+        header("Location: view.php?page_id=" . $page_id);
+        exit;
+
+    } catch (Exception $e) {
+        // An error occurred, rollback any database changes
+        $pdo->rollBack();
+        $error = 'Error creating the celestial body and page: ' . $e->getMessage();
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -86,36 +124,44 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         <h1>Create New Page</h1>
         
         <?php if ($error): ?>
-            <p class="error-message"><?php echo $error; ?></p>
+            <p class="error"><?php echo $error; ?></p>
         <?php endif; ?>
-
+        
         <?php if ($success): ?>
-            <p class="success-message"><?php echo $success; ?></p>
+            <p class="success"><?php echo $success; ?></p>
         <?php endif; ?>
 
         <form action="create_page.php" method="post" enctype="multipart/form-data">
-            <label for="title">Name:</label>
-            <input type="text" id="title" name="title" required>
+            <div>
+                <label for="name">Name:</label>
+                <input type="text" id="name" name="name" required>
+            </div>
             
-            <label for="category">Category:</label>
-            <select id="category" name="category" required>
-                <?php foreach ($categories as $category): ?>
-                    <option value="<?php echo $category['category_id']; ?>">
-                        <?php echo htmlspecialchars($category['category_name']); ?>
-                    </option>
-                <?php endforeach; ?>
-            </select>
+            <div>
+                <label for="category">Category:</label>
+                <select id="category" name="category" required>
+                    <?php foreach ($categories as $category): ?>
+                        <option value="<?php echo $category['category_id']; ?>">
+                            <?php echo htmlspecialchars($category['category_name']); ?>
+                        </option>
+                    <?php endforeach; ?>
+                </select>
+            </div>
             
-            <label for="content">Description:</label>
-            <textarea id="content" name="content" required></textarea>
+            <div>
+                <label for="description">Description:</label>
+                <textarea id="description" name="description" required></textarea>
+            </div>
             
-            <label for="image">Upload Image (optional):</label>
-            <input type="file" id="image" name="image">
+            <div>
+                <label for="image">Upload Image (optional):</label>
+                <input type="file" id="image" name="image">
+            </div>
             
-            <input type="submit" value="Create Page">
+            <div>
+                <input type="submit" value="Create Page">
+            </div>
         </form>
     </main>
-
-    <!-- ... footer ... -->
 </body>
 </html>
--- planet_cms.sql
+++ planet_cms.sql
@@ -1,46 +1,280 @@
--- 创建用户表
-CREATE TABLE users (
-  user_id INT AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  user_type ENUM('admin', 'visitor') NOT NULL
-);
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- 主机： localhost
+-- 生成日期： 2023-11-09 02:37:02
+-- 服务器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
 
--- 创建评论表
-CREATE TABLE comments (
-  comment_id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT NOT NULL,
-  celestial_body_id INT NOT NULL,
-  content TEXT NOT NULL,
-  creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(user_id),
-  FOREIGN KEY (celestial_body_id) REFERENCES celestial_bodies(celestial_body_id)
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
--- 创建分类表
-CREATE TABLE categories (
-  category_id INT AUTO_INCREMENT PRIMARY KEY,
-  category_name ENUM('Terrestrial Planets', 'Gas Giants', 'Ice Giants', 'Dwarf Planets', 'Exoplanets') NOT NULL
-);
 
--- 创建天体表
-CREATE TABLE celestial_bodies (
-  celestial_body_id INT AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  category_id INT,
-  description TEXT,
-  discovery_date DATE,
-  image_url VARCHAR(255),
-  FOREIGN KEY (category_id) REFERENCES categories(category_id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
--- 创建页面表
-CREATE TABLE pages (
-  page_id INT AUTO_INCREMENT PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
-  content TEXT NOT NULL,
-  creator_id INT NOT NULL,
-  creation_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-  last_modified_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  FOREIGN KEY (creator_id) REFERENCES users(user_id)
-);
+--
+-- 数据库： `SolarSystemCMS`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `categories`
+--
+
+CREATE TABLE `categories` (
+  `category_id` int(11) NOT NULL,
+  `category_name` enum('Terrestrial Planets','Gas Giants','Ice Giants','Dwarf Planets','Exoplanets') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `categories`
+--
+
+INSERT INTO `categories` (`category_id`, `category_name`) VALUES
+(1, 'Terrestrial Planets'),
+(2, 'Gas Giants'),
+(3, 'Ice Giants'),
+(4, 'Dwarf Planets');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `celestial_bodies`
+--
+
+CREATE TABLE `celestial_bodies` (
+  `celestial_body_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `celestial_bodies`
+--
+
+INSERT INTO `celestial_bodies` (`celestial_body_id`, `name`, `category_id`, `description`, `image_url`) VALUES
+(8, 'Mercury', 1, 'The smallest planet in the Solar System and the closest to the Sun. It has a cratered surface and no atmosphere to support life as we know it.', 'uploads/Mercury.jpg'),
+(9, '1', 1, '1', NULL),
+(10, 'Mercury', 1, 'The smallest planet in the Solar System and the closest to the Sun. It has a cratered surface and no atmosphere to support life as we know it.', 'uploads/Mercury.jpg'),
+(11, '1', 1, '1', NULL),
+(12, '1', 1, '1', 'uploads/Uranus.png'),
+(13, '2', 1, '2', 'uploads/space-background.jpg'),
+(14, 'test', 1, '1', 'uploads/Venus.jpg'),
+(15, '122', 1, '1', 'uploads/saturn.png');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `comments`
+--
+
+CREATE TABLE `comments` (
+  `comment_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `celestial_body_id` int(11) NOT NULL,
+  `content` text NOT NULL,
+  `creation_time` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `images`
+--
+
+CREATE TABLE `images` (
+  `id` int(11) NOT NULL,
+  `page_id` int(11) DEFAULT NULL,
+  `file_name` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `images`
+--
+
+INSERT INTO `images` (`id`, `page_id`, `file_name`) VALUES
+(5, NULL, 'Mercury.jpg'),
+(6, NULL, 'Mercury.jpg'),
+(7, NULL, 'Uranus.png'),
+(8, NULL, 'space-background.jpg'),
+(9, 23, 'Venus.jpg'),
+(10, NULL, 'saturn.png');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `pages`
+--
+
+CREATE TABLE `pages` (
+  `page_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `creation_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `last_modified_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `category_id` int(11) DEFAULT NULL,
+  `creator_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `pages`
+--
+
+INSERT INTO `pages` (`page_id`, `title`, `content`, `creation_time`, `last_modified_time`, `category_id`, `creator_id`) VALUES
+(17, 'Mercury', 'The smallest planet in the Solar System and the closest to the Sun. It has a cratered surface and no atmosphere to support life as we know it.', '2023-11-08 20:17:03', '2023-11-08 20:17:03', 1, 1),
+(18, '1', '1', '2023-11-08 20:23:10', '2023-11-08 20:23:10', 1, 1),
+(19, 'Mercury', 'The smallest planet in the Solar System and the closest to the Sun. It has a cratered surface and no atmosphere to support life as we know it.', '2023-11-08 20:27:54', '2023-11-08 20:27:54', 1, 1),
+(20, '1', '1', '2023-11-08 20:28:05', '2023-11-08 20:28:05', 1, 1),
+(21, '1', '1', '2023-11-08 20:29:30', '2023-11-08 20:29:30', 1, 1),
+(22, '2', '2', '2023-11-08 20:29:49', '2023-11-08 20:29:49', 1, 1),
+(23, 'test', '1', '2023-11-08 20:32:29', '2023-11-08 20:32:29', 1, 1),
+(24, '122', '1', '2023-11-08 20:42:50', '2023-11-08 20:42:50', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','visitor') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- 转存表中的数据 `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `password`, `role`) VALUES
+(1, '18188499883', '$2y$10$hnOxRg/xTNL4Yt6MAcrVnOkqiVgLp/TyqIs2aeYIxeAwPskuyR3ee', 'admin');
+
+--
+-- 转储表的索引
+--
+
+--
+-- 表的索引 `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`category_id`);
+
+--
+-- 表的索引 `celestial_bodies`
+--
+ALTER TABLE `celestial_bodies`
+  ADD PRIMARY KEY (`celestial_body_id`),
+  ADD KEY `category_id` (`category_id`);
+
+--
+-- 表的索引 `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`comment_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `celestial_body_id` (`celestial_body_id`);
+
+--
+-- 表的索引 `images`
+--
+ALTER TABLE `images`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `page_id` (`page_id`);
+
+--
+-- 表的索引 `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`page_id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `creator_id` (`creator_id`);
+
+--
+-- 表的索引 `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `category_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- 使用表AUTO_INCREMENT `celestial_bodies`
+--
+ALTER TABLE `celestial_bodies`
+  MODIFY `celestial_body_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- 使用表AUTO_INCREMENT `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `comment_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- 使用表AUTO_INCREMENT `images`
+--
+ALTER TABLE `images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- 使用表AUTO_INCREMENT `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `page_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- 使用表AUTO_INCREMENT `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- 限制导出的表
+--
+
+--
+-- 限制表 `celestial_bodies`
+--
+ALTER TABLE `celestial_bodies`
+  ADD CONSTRAINT `celestial_bodies_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`);
+
+--
+-- 限制表 `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`celestial_body_id`) REFERENCES `celestial_bodies` (`celestial_body_id`);
+
+--
+-- 限制表 `images`
+--
+ALTER TABLE `images`
+  ADD CONSTRAINT `images_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`page_id`);
+
+--
+-- 限制表 `pages`
+--
+ALTER TABLE `pages`
+  ADD CONSTRAINT `fk_creator_id` FOREIGN KEY (`creator_id`) REFERENCES `users` (`user_id`),
+  ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`creator_id`) REFERENCES `users` (`user_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
